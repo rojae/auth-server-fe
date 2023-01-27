84deb364cacc3a0cba170984a75970da5992bb91
rojae <rojae@kakao.com>
2023-01-28 05:08:49 +0900

    add script

---
+++ script/boot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./gradlew clean build
+
+java -jar index-web/build/libs/index-web-0.0.1-SNAPSHOT.jar > index-web/log.out 2>&1 &
+java -jar auth-signin-web/build/libs/auth-signin-web-0.0.1-SNAPSHOT.jar > auth-signin-web/log.out 2>&1 &
+java -jar auth-signup-web/build/libs/auth-signup-web-0.0.1-SNAPSHOT.jar > auth-signup-web/log.out 2>&1 &
+
