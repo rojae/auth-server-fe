f2e34681a6cec0c8c01b05631a60a6aeaf9a5923
rojae <rojae@kakao.com>
2023-03-18 19:43:24 +0900

    del. xss filter test controller

--- auth-signin-web/src/main/java/io/github/rojae/authsigninweb/controller/XssParam.java
+++
@@ -1,9 +0,0 @@
-package io.github.rojae.authsigninweb.controller;
-
-import lombok.Data;
-
-@Data
-public class XssParam {
-    private String content;
-    private String taskName;
-}
--- auth-signup-web/src/main/java/io/github/rojae/authsignupweb/controller/XssParam.java
+++
@@ -1,9 +0,0 @@
-package io.github.rojae.authsignupweb.controller;
-
-import lombok.Data;
-
-@Data
-public class XssParam {
-    private String content;
-    private String taskName;
-}
